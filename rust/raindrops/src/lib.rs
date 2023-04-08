b7486336c8173c7035b7e6eb4735e3dbf9e95b27
artogahr <artogahr@gmail.com>
2023-04-08 22:41:01 +0200

    Raindrops improved

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,19 +1,15 @@
 pub fn raindrops(n: u32) -> String {
-    let mut flag: bool = false;
     let mut result: String = String::new();
     if n % 3 == 0 {
         result.push_str("Pling");
-        flag = true;
     }
     if n % 5 == 0 {
         result.push_str("Plang");
-        flag = true;
     }
     if n % 7 == 0 {
         result.push_str("Plong");
-        flag = true;
     }
-    if flag {
+    if !result.is_empty() {
         result
     } else {
         n.to_string()
