222def924fa1b663b11dcb378da0b64797ae6fce
artogahr <artogahr@gmail.com>
2023-04-20 00:08:48 +0200

    Diffie-Hellman

--- rust/diffie-hellman/src/lib.rs
+++ rust/diffie-hellman/src/lib.rs
@@ -13,21 +13,18 @@ pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
 }
 
 pub fn modular_exp(base: u64, exponent: u64, modulus: u64) -> u64 {
-    if modulus == 1 {
-        return 0;
-    }
-    let mut result = 1_u128;
-    let mut e = exponent as u128;
     let mut b = base as u128;
+    let mut e = exponent as u128;
     let m = modulus as u128;
-    assert_ne!((m - 1).checked_mul(m - 1), None);
+    if m == 1 {return 0}
+    let mut result: u128 = 1;
     b = b % m;
     while e > 0 {
         if e % 2 == 1 {
             result = (result * b) % m;
         }
         e = e >> 1;
-        b = (b * b) % m;
+        b = (b*b) % m;
     }
     result as u64 % modulus
 }
