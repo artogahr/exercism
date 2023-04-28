138833980e0f152ac602752aa22cef7ff27906fa
artogahr <artogahr@gmail.com>
2023-04-28 22:13:04 +0200

    Hamming

---
+++ rust/hamming/src/lib.rs
@@ -0,0 +1,13 @@
+/// Return the Hamming distance between the strings,
+/// or None if the lengths are mismatched.
+pub fn hamming_distance(s1: &str, s2: &str) -> Option<usize> {
+    if s1.len() != s2.len() { return None; }
+
+    let mut distance: usize = 0;
+    for i in 0..s1.len() {
+        if s1.chars().nth(i) != s2.chars().nth(i) {
+            distance += 1;
+        }
+    }
+    Some(distance)
+}
