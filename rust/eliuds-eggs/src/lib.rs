614f266a70f5a537037f046f4d41d5f1e31defe5
Artoghrul Gahramanli <artogahr@gmail.com>
2024-07-05 18:53:36 +0200

    Eluid's Eggs v2

--- rust/eliuds-eggs/src/lib.rs
+++ rust/eliuds-eggs/src/lib.rs
@@ -1,12 +1,9 @@
 pub fn egg_count(display_value: u32) -> usize {
-    let mut temp = display_value;
-    let mut count = 0;
-    for nth_power in (0..32).rev() {
-        let divisor = (2 as u32).pow(nth_power);
-        if temp / divisor == 1 {
-            temp = temp - divisor;
-            count += 1;
-        }
-    }
-    count
+    (0..32)
+        .rev()
+        .filter(|&nth_power| {
+            let divisor = 2u32.pow(nth_power);
+            display_value / divisor % 2 == 1
+        })
+        .count()
 }
